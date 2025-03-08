37ffaac6d60557fd5987d908fe8d4ffd2bb7491b
Maxim Tampere <maxim.tampere@student.howest.be>
2025-03-08 13:05:32 +0100

    favicon added and bill drop down functional

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 const Bill = ({tableNumber, dataClient}) => {
 
     let status = "Waiting"
+
     switch (dataClient.statusNumber) {
         case 1:
             status = "Ready to order Drinks"
@@ -27,6 +30,21 @@ const Bill = ({tableNumber, dataClient}) => {
         default:
             break;
     }
+
+    const [isOpen, setIsOpen] = useState(true);
+    const handleClickDropDown = (e) => {
+        const billTag = e.target.parentElement.parentElement
+        if(isOpen === true){
+            billTag.querySelector(".bill__list").classList.add("visually-hidden")
+            billTag.querySelector(".bill__head button").classList.remove("upsidedown")
+            setIsOpen(false)
+        } else {
+            billTag.querySelector(".bill__list").classList.remove("visually-hidden")
+            billTag.querySelector(".bill__head button").classList.add("upsidedown")
+            setIsOpen(true)
+        }
+    }
+
     return (
         <li className="bill">
             <div className="bill__head">
@@ -34,7 +52,7 @@ const Bill = ({tableNumber, dataClient}) => {
                     <h3>Table {tableNumber}</h3>
                     <p>Status: {status}</p>
                 </div>
-                <button>v</button>
+                <button className="upsidedown" onClick={handleClickDropDown}>v</button>
             </div>
             <ul className="bill__list">
                 {dataClient.consumentProducts.map((product, index) => (
